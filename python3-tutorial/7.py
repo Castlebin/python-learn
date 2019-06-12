efa89893b9909b318855b640d767e3726649ddaa
Castlebin <Castlebin>
2019-06-12 16:54:50 +0800

    7.文件和io

--- python3-tutorial/7.py
+++ python3-tutorial/7.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+f = open("6.py")
+print(f.read())
+f.close()
+
+f = open("1.log", "w")
+f.write("This is a log line")
+f.close()
+
+# with子句，用完文件自动关闭，不用显式调用close()方法
+with open("5.py") as f:
+    print(f.read())
+print(f.closed)
+

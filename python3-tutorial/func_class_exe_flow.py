44682529e7dd275b6591c32cb3341ac9a96e1b77
Castlebin <Castlebin>
2019-06-17 17:15:45 +0800

    理解一下python代码的执行流程

---
+++ python3-tutorial/func_class_exe_flow.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 理解一下python的代码执行过程
+'''  result:
+1
+2
+myclass
+lssssss
+main
+'''
+
+
+print("1")
+
+
+def f1():
+    print("f1")
+
+
+print("2")
+
+def main():
+    print("main")
+
+
+class MyClass:
+    print("myclass")
+
+    def __init__(self):
+        print("MyClass init")
+
+    print("lssssss")
+
+
+if __name__ == '__main__':
+    main()
+
